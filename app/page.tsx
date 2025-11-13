bd694c444656a6ee8b643c46218648af42bc13d1
Core One <homestorzusa@gmail.com>
2025-11-12 21:46:21 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import LoginForm from '../components/LoginForm';
+import LoginForm from '@/components/auth/LoginForm';
 import { createClient } from '@/utils/supabase/client';
 
 export default function Page() {
